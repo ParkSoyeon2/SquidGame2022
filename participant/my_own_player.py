af0c04a64f7362728e80c28794c6b8d18c9b24ce
YunwonChoi <dbsdnjs7856@snu.ac.kr>
2022-06-15 05:18:33 +0900

    marble 완료

--- participant/my_own_player.py
+++ participant/my_own_player.py
@@ -2,7 +2,6 @@ import random
 from . import participant as part
 import copy
 
-
 class my_own_player(part.Participant):
     def __init__(self):
         super().__init__('name of your team', 'team num')
@@ -26,13 +25,14 @@ class my_own_player(part.Participant):
         # you can refer to an object of 'marbles', named as 'playground_marbles'
         # the return should be the number of marbles bet (> 0)!
         my_current_marbles = playground_marbles.get_num_of_my_marbles(self)
-        return random.randint(playground_marbles.MIN_HOLDING, my_current_marbles)
+        return 1
 
     def declare_statement_strategy(self, playground_marbles):
         # you can override this method in this sub-class
         # you can refer to an object of 'marbles', named as 'playground_marbles'
         # the return should be True or False!
-        answer = bool(random.randint(0, 1))
+        num = playground_marbles._marbles_in_hand
+        answer = bool(num % 2)
         return self.set_statement(answer)
     # ================================================================================= for marble game
 
