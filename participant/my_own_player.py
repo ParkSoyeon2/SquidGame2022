3c2298210e69f82bea87555216869fc3f0822283
ParkSoyeon2 <soyeon2@snu.ac.kr>
2022-06-17 12:44:08 +0900

    branch A, glass_stepping 시도

--- participant/my_own_player.py
+++ participant/my_own_player.py
@@ -1,6 +1,7 @@
 import random
 from . import participant as part
 import copy
+#from glass_stepping_stones.game import stage_map
 
 
 class my_own_player(part.Participant):
@@ -44,22 +45,23 @@ class my_own_player(part.Participant):
         # you can refer to an object of 'glass_stepping_stones', named as 'playground_glasses'
         # the return should be 0 or 1 (int)!
         if self.position == 0:
+            #self.temp_list = Map.hard_glasses
             self.temp_list = copy.deepcopy(playground_glasses._players_steps)  # 상대방것도 복사
         length = len(self.temp_list)
         if self.previous_player != 'None' and self.temp_list != []:
             if self.position < length - 1:  # 카피 한 것보다 앞에 있으면
                 print(self.temp_list)
-                # print('chk1')
+                print('chk1')
                 return self.temp_list[self.position]  # 내가 갔던 곳으로
             else:
                 if self.position == length - 1:
-                    # print('chk2')
+                    print('chk2')
                     if self.temp_list[self.position] == 0:
                         return 1
                     else:
                         return 0
                 else:
-                    # print('chk3')
+                    print('chk3')
                     return random.randint(0, 1)
         return random.randint(0, 1)
     # ================================================================================= for glass_stepping_stones game
